9622a6f63206ed1d617d1556dd7dd2213f3036ef
Ben <benjamin_clayman@alumni.brown.edu>
2013-03-19 15:36:21 -0400

    rough draft

--- app/assets/javascripts/squashsimulator.js
+++ app/assets/javascripts/squashsimulator.js
@@ -8,16 +8,102 @@ function start() {
         $('#team2').append('<option value="' + teamList[i] + '">' + teamList[i] + ' </option>');
       }
 
-  $("#slider1").slider();
+for (var n = 1; n < 10; n++) {
+  $("#slider1").slider({
+    min: 0,
+    max: 100,
+    slide: function(event, ui) {
+      $('#probability1').val(ui.value);
+      $('#team2probability1').val(100 - (ui.value));
+
+    }
+  });
+}
+  $("#slider2").slider({
+    min: 0,
+    max: 100,
+    slide: function(event, ui) {
+      $('#probability2').val(ui.value);
+      $('#team2probability2').val(100 - (ui.value));
+    }
+  });
+
+  $("#slider3").slider({
+    min: 0,
+    max: 100,
+    slide: function(event, ui) {
+      $('#probability3').val(ui.value);
+      $('#team2probability3').val(100 - (ui.value));
+    }
+  });
+
+  $("#slider4").slider({
+    min: 0,
+    max: 100,
+    slide: function(event, ui) {
+      $('#probability4').val(ui.value);
+      $('#team2probability4').val(100 - (ui.value));
+    }
+  });
+
+  $("#slider5").slider({
+    min: 0,
+    max: 100,
+    slide: function(event, ui) {
+      $('#probability5').val(ui.value);
+      $('#team2probability5').val(100 - (ui.value));
+    }
+  });
+
+  $("#slider6").slider({
+    min: 0,
+    max: 100,
+    slide: function(event, ui) {
+      $('#probability6').val(ui.value);
+      $('#team2probability6').val(100 - (ui.value));
+    }
+  });
+
+  $("#slider7").slider({
+    min: 0,
+    max: 100,
+    slide: function(event, ui) {
+      $('#probability7').val(ui.value);
+      $('#team2probability7').val(100 - (ui.value));
+    }
+  });
+
+  $("#slider8").slider({
+    min: 0,
+    max: 100,
+    slide: function(event, ui) {
+      $('#probability8').val(ui.value);
+      $('#team2probability8').val(100 - (ui.value));
+    }
+  });
+
+  $("#slider9").slider({
+    min: 0,
+    max: 100,
+    slide: function(event, ui) {
+      $('#probability9').val(ui.value);
+      $('#team2probability9').val(100 - (ui.value));
+    }
+  });
+
 }
 
 function reset() {
   for (var l = 1; l < 10; l++) {
     $('#probability' + l).val('');
+    $('#team2probability' + l).val('');
   }
   $('#chart').addClass('hide');
   $('#simulate_button').show();
   $('#reset_button').addClass('hide');
+  $('#result').hide();
+  $('#after').show();
+  $('#instructions').show()
 }
 
 function run_simulation() {
@@ -88,25 +174,13 @@ function run_simulation() {
     }
 
     var output = "Over " + num_of_simulations.toString() + " simulations, " + $('#team1').val() + ": " + totalMatches1.toString() + ", " + $('#team2').val() + ": " + totalMatches2.toString() + ". " +
-    "The probability of " + $('#team1').val() + " winning is " + ((totalMatches1/num_of_simulations)*100).toFixed(2) + "%." + " The probability of " + $('#team2').val() + " winning is " + ((totalMatches2/num_of_simulations)*100).toFixed(2) + "%."
-
-    var breakdown9 = " Team 1 wins 9-0: " + ((team1_90/num_of_simulations)*100).toFixed(2) + "%";
-    var breakdown8 = " Team 1 wins 8-1: " + ((team1_81/num_of_simulations)*100).toFixed(2) + "%";
-    var breakdown7 = " Team 1 wins 7-2: " + ((team1_72/num_of_simulations)*100).toFixed(2) + "%";
-    var breakdown6 = " Team 1 wins 6-3: " + ((team1_63/num_of_simulations)*100).toFixed(2) + "%";
-    var breakdown5 = " Team 1 wins 5-4: " + ((team1_54/num_of_simulations)*100).toFixed(2) + "%";
-    var breakdown4 = " Team 1 loses 4-5: " + ((team1_45/num_of_simulations)*100).toFixed(2) + "%";
-    var breakdown3 = " Team 1 loses 3-6: " + ((team1_36/num_of_simulations)*100).toFixed(2) + "%";
-    var breakdown2 = " Team 1 loses 2-7: " + ((team1_27/num_of_simulations)*100).toFixed(2) + "%";
-    var breakdown1 = " Team 1 loses 1-8: " + ((team1_18/num_of_simulations)*100).toFixed(2) + "%";
-    var breakdown0 = " Team 1 loses 0-9: " + ((team1_09/num_of_simulations)*100).toFixed(2) + "%";
-
+    $('#team1').val() + " wins " + ((totalMatches1/num_of_simulations)*100).toFixed(2) + "%. " + $('#team2').val() + " wins " + ((totalMatches2/num_of_simulations)*100).toFixed(2) + "%."
     $('#result').text(output);
-
-
-    $('#result').removeClass('hide');
+    $('#result').show();
     $('#chart').removeClass('hide');
     $('#chart').empty();
+    $('#after').hide();
+    $('#instructions').hide();
 
   }
 
@@ -125,7 +199,6 @@ function run_simulation() {
         { x: '2-7', y: ((team1_27/num_of_simulations)*100).toFixed(2)},
         { x: '1-8', y: ((team1_18/num_of_simulations)*100).toFixed(2)},
         { x: '0-9', y: ((team1_09/num_of_simulations)*100).toFixed(2)}
-
       ],
       xkey: ['x'],
       ykeys: ['y'],
